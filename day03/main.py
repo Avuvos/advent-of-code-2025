4a3ad7db19f877bc28855cffc67b99916e4b642b
Avuvos <aviakad@mail.tau.ac.il>
2025-12-06 13:53:22 +0200

    small refactor day3

--- day03/main.py
+++ day03/main.py
@@ -5,16 +5,14 @@ def solve_part1(data: str) -> int:
     banks = data.splitlines()
 
     result = 0
-
     for bank in banks:
         max_digit = "0"
-        best = 0
-        for i, c in enumerate(bank):
-            if int(max_digit + c) > int(best):
-                best = max_digit + c
-            if int(c) > int(max_digit):
-                max_digit = c
-        result += int(best)
+        best_pair_value = 0
+        for c in bank:
+            if int(max_digit + c) > best_pair_value:
+                best_pair_value = int(max_digit + c)
+            max_digit = max(max_digit, c)
+        result += best_pair_value
 
     return result
 
@@ -24,19 +22,18 @@ def solve_part2(data: str) -> int:
 
     result = 0
     LEN = 12
-
     for bank in banks:
         max_digits = [c for c in bank[0:LEN]]
         best = int("".join(max_digits))
         for cur_digit in bank[LEN:]:
-            new_max_digits = max_digits
+            next_max_digits = max_digits
             for i in range(LEN):
-                cur_max_digits = max_digits[0 : i] + max_digits[i + 1:] + [cur_digit]
+                cur_max_digits = max_digits[0: i] + max_digits[i + 1:] + [cur_digit]
                 cur_val = int("".join(cur_max_digits))
                 if cur_val > best:
                     best = cur_val
-                    new_max_digits = cur_max_digits
-            max_digits = new_max_digits
+                    next_max_digits = cur_max_digits
+            max_digits = next_max_digits
         result += best
 
     return result
